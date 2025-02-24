0a2dc56ed632ccbd56058c14204b86a91bb7db29
Sumeeth <sumeeth.dravida@gmail.com>
2025-02-24 20:13:56 +0530

    Signup and signin method created completely

--- Social_Media_Project.py
+++ Social_Media_Project.py
@@ -24,15 +24,27 @@ class EightMate:
         else:
             exit()
         
-    def signup (self):
-        email = input("Enter you email here: ")
-        password = input("Enter your password here: ")
+    def signup(self):
+        email = input("Enter you username/email here: ")
+        password = input("Setup your password here: ")
         self.username = email
         self.password = password
         print("You have signed up successfully!")
         print("\n")
         self.menu()
         
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("Please sign up first by pressing 1 in the main menu")
+        else:
+            uname = input("Enter username/email here: ")
+            pwd = input("Enter your password here: ")
+            if self.username==uname and self.password == pwd:
+                print("You have signed in successfully!")
+            self.loggedin = True
+
+    
+
 
 
 obj = EightMate()
